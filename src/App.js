1c19d21b591786badb51c674c50870d3d1bad28d
MunawarRaza <munawarraza068@gmail.com>
2024-06-10 14:24:59 +0500

    add new line

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
 	<p>
-	  Welcome to my First React App.
+	  Welcome to my First React App. This is my first app
 	</p>
         <a
           className="App-link"
