06444b92da8c38ff7e5cb04055ca914e7f283474
MunawarRaza <munawarraza068@gmail.com>
2024-06-10 13:03:36 +0500

    push

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
 	<p>
-	  Welcome to my First App
+	  Welcome to my First App of React
 	</p>
         <a
           className="App-link"
